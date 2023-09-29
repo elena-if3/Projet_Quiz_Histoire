7dadcf12db40c93825f07e93473d8c4ef295fc2b
Elena Rottin <elena.rottin@gmail.com>
2023-09-29 16:35:25 +0200

    test views base+1+2 + Bootstrap card added

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,14 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+// add bootstrap .css
+import 'bootstrap/dist/css/bootstrap.css';
+
+// add jquery
+const $ = require ('jquery');
+window.jQuery = $;
+window.$ = $;
+
+// importer bootstrap
+import 'bootstrap';
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/vue1', name: 'vue 1')]
+    public function vue1(): Response
+    {
+        return $this->render('home/vue1.html.twig');
+    }
+
+    #[Route('/vue2', name: 'vue 2')]
+    public function vue2(): Response
+    {
+        return $this->render('home/vue2.html.twig');
+    }
+}
