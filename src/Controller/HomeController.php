dcf69a16faf5a7e517b5d19a8614cef80fb782ba
Elena Rottin <elena.rottin@gmail.com>
2023-10-02 16:27:15 +0200

    bootstrap card displaying quiz proposition ok / random choice ok!

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\QuizItem;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('home/index.html.twig');
+        $allItems = $doctrine->getRepository(QuizItem::class);
+        $allItemsArray = $allItems->findAll();
+
+        $quizPropositionIndex = mt_rand(0, count($allItemsArray)-1);
+        $quizProposition = $allItemsArray[$quizPropositionIndex];
+
+        $var = ['quizProposition' => $quizProposition];
+
+        return $this->render('home/index.html.twig', $var);
     }
 
     #[Route('/vue1', name: 'vue 1')]
